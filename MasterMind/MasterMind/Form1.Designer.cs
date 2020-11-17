65a31d68c927c1a51dd038ad651e0d3ce2ff35e1
billblap <65726627+billboblap@users.noreply.github.com>
2020-11-17 09:15:52 -0800

    Add files via upload

---
+++ MasterMind/MasterMind/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MasterMind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(39, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(65, 39);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(91, 39);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MasterMind Bill Barg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(128, 85);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MasterMind/MasterMind/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterMind
+{
+    public partial class Form1 : Form
+   {
+        string[] numbers = new string[4];
+        Random rndnum = new Random();
+        bool match1 = false;
+        bool match2 = false;
+        bool match3 = false;
+        bool match4 = false;
+        int iterations = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Red;
+            pictureBox2.BackColor = Color.Red;
+            pictureBox3.BackColor = Color.Red;
+            pictureBox4.BackColor = Color.Red;
+            for (int i = 0; i < 4; i++)
+            {
+                int num = rndnum.Next(1, 9);
+                numbers[i] = Convert.ToString(num);
+                
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        void calculate() 
+        {
+            iterations = 0;
+            try
+            {
+                if (textBox1.TextLength == 4)
+                {
+                    if ((numbers[0]) == textBox1.Text.Substring(0,1)) 
+                    {
+                        //pictureBox1.BackColor = Color.Green;
+                        iterations++;
+                    }
+                    if ((numbers[1]) == textBox1.Text.Substring(1, 1))
+                    {
+                        //pictureBox2.BackColor = Color.Green;
+                        iterations++;
+                    }
+                    if ((numbers[2]) == textBox1.Text.Substring(2, 1))
+                    {
+                        //pictureBox3.BackColor = Color.Green;
+                        iterations++;
+                    }
+                    if ((numbers[3]) == textBox1.Text.Substring(3, 1))
+                    {
+                        //pictureBox4.BackColor = Color.Green;
+                        iterations++;
+                    }
+                    if ((numbers[0]) == textBox1.Text.Substring(0, 1) & (numbers[1]) == textBox1.Text.Substring(1, 1) & (numbers[2]) == textBox1.Text.Substring(2, 1) & (numbers[3]) == textBox1.Text.Substring(3, 1)) 
+                    {
+                        MessageBox.Show("You Win!");
+                    }
+                    
+                    switch (iterations) 
+                    {
+                        
+                        case (1):
+                            pictureBox1.BackColor = Color.Green;
+                            pictureBox2.BackColor = Color.Red;
+                            pictureBox3.BackColor = Color.Red;
+                            pictureBox4.BackColor = Color.Red;
+                            break;
+                        case (2):
+                            pictureBox1.BackColor = Color.Green;
+                            pictureBox2.BackColor = Color.Green;
+                            pictureBox3.BackColor = Color.Red;
+                            pictureBox4.BackColor = Color.Red;
+                            break;
+                        case (3):
+                            pictureBox1.BackColor = Color.Green;
+                            pictureBox2.BackColor = Color.Green;
+                            pictureBox3.BackColor = Color.Green;
+                            pictureBox4.BackColor = Color.Red;
+                            break;
+                        case (4):
+                            pictureBox1.BackColor = Color.Green;
+                            pictureBox2.BackColor = Color.Green;
+                            pictureBox3.BackColor = Color.Green;
+                            pictureBox4.BackColor = Color.Green;
+                            break;
+                    }
+                }
+                if(textBox1.TextLength > 4)
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+                    pictureBox1.BackColor = Color.Red;
+                    pictureBox2.BackColor = Color.Red;
+                    pictureBox3.BackColor = Color.Red;
+                    pictureBox4.BackColor = Color.Red;
+                }
+                if (textBox1.Text.Length < 4) 
+                {
+                    pictureBox1.BackColor = Color.Red;
+                    pictureBox2.BackColor = Color.Red;
+                    pictureBox3.BackColor = Color.Red;
+                    pictureBox4.BackColor = Color.Red;
+                }
+            }
+            catch 
+            {
+                return;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
